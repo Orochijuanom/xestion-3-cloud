df5089591530fedcd2f5d561f55472c71a0073b8
Juan Sebastian Cruz Perdomo <jcruz02@itfip.edu.co>
2016-12-04 18:16:50 -0500

    Revert "arreglos merge"
    This reverts commit f416f051fcc96a8c85414473fed3a33618fcdd50.

---
+++ database/migrations/2014_10_12_120000_create_t3_detalletable.php
@@ -0,0 +1,50 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateT3DetalleTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('t3_detalle', function (Blueprint $table) {
+            $table->increments('id');            
+            $table->string('norma');
+            $table->string('fecha_publicacion');
+            $table->string('emisor');
+            $table->string('articulo');
+            $table->string('descripcion_obligacion');
+            $table->string('palabra_clave');
+            $table->string('aplica');
+            $table->string('cumple');
+            $table->string('como_cumple');
+            $table->string('responsable_cumplimiento');
+            
+
+            $table->integer('t3_id')->unsigned();
+            $table->foreign('t3_id')
+                  ->references('id')->on('t3')
+                  ->onUpdate('no action')
+                  ->onDelete('restrict');
+                                                                                    
+            $table->timestamp('created_at')->nullable();
+            $table->timestamp('updated_at')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('t3_detalle');
+    }
+}
